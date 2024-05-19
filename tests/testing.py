1e639d9e703e4d243c64eefe36eb2ce73a7a19df
Riccardo Galafassi <rigal@live.it>
2024-05-19 13:59:03 +0200

    Added simple tests

---
+++ tests/testing.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+	from expy.experiment import Experiment
+	ex = Experiment("test")
+	ex.load_data("tests/data", folder = True, extension = ".dat")
+	ex.load_peaks("tests/data")
