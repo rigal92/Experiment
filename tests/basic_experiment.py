7695f04823e2dc1d5d4a53d98d8a538842f30ae7
Riccardo Galafassi <rigal@live.it>
2024-05-21 13:08:35 +0200

    Unmonitoring tests folder

--- tests/basic_experiment.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-	from expy.experiment import Experiment
-
-	ex = Experiment("test")
-	ex.load_data("tests/data", folder = True, extension = ".dat")
-	ex.load_peaks("tests/data")
