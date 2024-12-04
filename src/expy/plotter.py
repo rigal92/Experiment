2c5f4dc28aae4ed7bc642071dd733077c26bee37
Riccardo Galafassi <rigal@live.it>
2024-12-04 15:26:23 +0100

    modified plotter descriptor

--- src/expy/plotter.py
+++ src/expy/plotter.py
@@ -43,9 +43,10 @@ def plot_event(
         keywords.
         - Missing keys will be assigned to None values
         - None will not normalize the data
-    bg_pattern: str (regular expression), default: None
+    bg_pattern: str (regular expression), numeric default: None
         - if **str** finds background columns matching the string 
         (see event_operations.background)
+        - if **numeric** subtract a constant value
         - if **None** no bg substraction
     to_background: None, str or list, default: ["y","ftot"]
         Column names to which the background will be substracted.
